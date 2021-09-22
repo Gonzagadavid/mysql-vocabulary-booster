eb316b10c19f04a9c39e8b00c18fd60f37a35c5e
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-09-21 22:45:26 -0300

    Criado uma procedure chamada buscar_media_por_cargo que recebe como parâmetro o nome de um cargo e em retorno deve mostrar a média salarial de todas as pessoas que possuem esse cargo, conforme especificado no requisito 15

---
+++ desafio15.sql
@@ -0,0 +1,18 @@
+USE hr;
+DELIMITER //
+
+CREATE PROCEDURE buscar_media_por_cargo(IN _cargo VARCHAR(50))
+BEGIN
+ SELECT 
+    ROUND(AVG(e.SALARY), 2) as 'Média salarial'
+FROM
+    hr.employees AS e
+        INNER JOIN
+    hr.jobs AS j ON j.JOB_ID = e.JOB_ID
+WHERE
+    j.JOB_TITLE = _cargo;
+END //
+
+DELIMITER ;
+
+CALL buscar_media_por_cargo('Programmer');
--- tests/hrChallenges.spec.js
+++ tests/hrChallenges.spec.js
@@ -92,7 +92,7 @@ describe('Desafios iniciais', () => {
     });
   });
 
-  describe.only('12 - Faça um relatório que lista todas as pessoas funcionárias **que possuem o mesmo cargo**', () => {
+  describe('12 - Faça um relatório que lista todas as pessoas funcionárias **que possuem o mesmo cargo**', () => {
     it('Verifica o desafio 12', async () => {
       const challengeQuery = readFileSync('desafio12.sql', 'utf8').trim();
       const expectedResult = require('./challengesResults/challengeResult12');
@@ -101,7 +101,7 @@ describe('Desafios iniciais', () => {
     });
   });
 
-  describe('15 - Crie uma procedure chamada `buscar_media_por_cargo` que recebe como parâmetro o nome de um cargo e em retorno deve mostrar a média salarial de todas as pessoas que possuem esse cargo', () => {
+  describe.only('15 - Crie uma procedure chamada `buscar_media_por_cargo` que recebe como parâmetro o nome de um cargo e em retorno deve mostrar a média salarial de todas as pessoas que possuem esse cargo', () => {
     it('Verifica o desafio 15', async () => {
       const challengeQuery = readFileSync('desafio15.sql', 'utf8').trim();
       const createProcedureQuery = /CREATE PROCEDURE.*END/si.exec(challengeQuery)[0];
